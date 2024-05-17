2ef7de5d6f61e7763cf4a907ecf9efe665397133
david sureway <nwachukwudavid100@gmail.com>
2024-05-17 22:15:29 +0100

    counter app from android mobile

--- Newfolder/web/springboot/HelloWorld/src/main/java/com/example/HelloWorld/CourseController.java
+++ Newfolder/web/springboot/HelloWorld/src/main/java/com/example/HelloWorld/CourseController.java
@@ -14,7 +14,10 @@ public class CourseController {
 	public List<Course> retrieveAllCourses(){
 		return Arrays.asList(
 				new Course(1, "Learn Aws", "in28Minutes"),
-				new Course(2, "Learn DevOps", "in28Minutes")
+				new Course(2, "Learn DevOps", "in28Minutes"), 
+				new Course(3, "Learn Azure", "in28Minutes"),
+				new Course(4, "Learn xxxxx", "in28Minutes"),
+				new Course(5, "Learn GCP", "in28Minutes")
 				);
 	}
 	
